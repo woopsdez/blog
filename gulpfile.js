3c13a634eb820d1ad1f668eb98cb3b69bbf48665
Miki Ishijima <woops.mirai@gmail.comm>
2016-05-04 12:38:07 +0900

    favicon, imagemin, _config_dev.ymlのurl

--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,7 @@ gulp.task('jekyll-build', function (done) {
     console.log(option['e']);
     console.log(config.option, config.yml);
     browserSync.notify(messages.jekyllBuild);
-    return cp.spawn( jekyll , ['build', config.option, config.yml, '--drafts'], {stdio: 'inherit'})
+    return cp.spawn( jekyll , ['build', config.option, config.yml], {stdio: 'inherit'})
         .on('close', done);
 });
 
@@ -40,7 +40,7 @@ gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
 });
 
 /**
- * Deploy the Jekyll Site
+ * Deploy the Jekyll Site 現在使われておりません
  */
 
 // parse command line argument
@@ -91,10 +91,10 @@ gulp.task('image', function() {
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
  */
 gulp.task('sass', function () {
-    return gulp.src('assets/css/*.scss')
+    return gulp.src('assets/css/screen.scss')
         .pipe(sass())
         .pipe(gulp.dest('assets/css'))
-        .pipe(gulp.dest('_site/assets/css'))
+        .pipe(gulp.dest('_site/assets/css/'))
         .pipe(browserSync.reload({stream:true}))
 });
 
@@ -111,7 +111,7 @@ gulp.task('watch', function () {
  * Default task, running just `gulp` will compile the sass,
  * compile the jekyll site, launch BrowserSync & watch files.
  */ 
-gulp.task('default', ['browser-sync', 'watch']);
+gulp.task('default', ['browser-sync', 'watch','image']);
 
 // 本番環境にのみ残しておきたい記事があるため、完全同期のhookではなく、ファイル追記のみ行うglynnでデプロイします。
 // gulp.task('deploy', function (callback) {
