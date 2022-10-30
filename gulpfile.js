aa4fdf343a4225899a0cc9cd8b2a11d01a069edd
tai2 <hello@tai2.net>
2022-10-30 23:39:46 +0900

    Migrate gulp configurations to version 5 format

--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,69 @@
-var gulp        = require('gulp');
-var sass        = require('gulp-sass');
-var prefix      = require('gulp-autoprefixer');
-var git         = require('gulp-git');
-var imageResize = require('gulp-image-resize');
-var browserSync = require('browser-sync');
-var cp          = require('child_process');
-var minimist    = require('minimist');
-var runSequence = require('run-sequence');
+const gulp        = require('gulp');
+const { src, watch, series, parallel } = require('gulp');
+const dartSass        = require('sass');
+const gulpSass        = require('gulp-sass');
+const sass = gulpSass(dartSass)
+const prefix      = require('gulp-autoprefixer');
+const git         = require('gulp-git');
+const imageResize = require('gulp-image-resize');
+const browserSync = require('browser-sync');
+const cp          = require('child_process');
+const minimist    = require('minimist');
+const runSequence = require('run-sequence');
 const del       = require('del')
 const imagemin  = require('gulp-imagemin');
 const pngquant  = require('imagemin-pngquant');
 
-var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+const jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
 
 /**
  * Build the Jekyll Site
  */
-var messages = {jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'};
-// var option = minimist(process.argv.slice(2));
-// var config = {localBuild: '_config.yml,_config_dev.yml'};
+const messages = {jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'};
+// const option = minimist(process.argv.slice(2));
+// const config = {localBuild: '_config.yml,_config_dev.yml'};
 // if (option['e'] === 'production') {
 //     config = {option:'', yml: ''};
 // } else {
 //     config = {option:'--config' ,yml: '_config.yml,_config_dev.yml'};
 // };
 
-gulp.task('jekyll-build', function (done) {
-    // console.log(option['e']);
-    // console.log(config.option, config.yml);
+function jekyllBuildTask(cb) {
     browserSync.notify(messages.jekyllBuild);
-    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
-        .on('close', done);
-});
+    return cp.spawn( jekyll , ['build', '--baseurl', ''], {stdio: 'inherit'})
+        .on('close', cb);
+}
 
 /**
  * Rebuild Jekyll & do page reload
  */
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
-    browserSync.reload();
-});
+function jekyllRebuildTask(cb) {
+	browserSync.reload();
+	cb();
+}
 
 /**
  * Wait for jekyll-build, then launch the Server
  */
-gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
-    browserSync({
-        server: {
-            baseDir: '_site'
-        }
-    });
-});
+function browserSyncTask(cb) {
+	browserSync({
+		server: {
+			baseDir: '_site'
+		}
+	});
+	cb()
+}
 
 /**
  * Resize & Optimize Image
- */ 
-gulp.task('del-image', function() {
-    del(['_site/content/']);
-});
+ */
+function delImageTask(cb) {
+	del(['_site/content/']);
+	cb();
+}
 
-gulp.task('image',['del-image'], function() {
-    gulp.src('content/images/**/**/**/*.{jpg,png}', {base:'content/images/'})
+function imageTask(cb) {
+    src('content/images/**/**/**/*.{jpg,png}', {base:'content/images/'})
     .pipe(imageResize({width : 1000}))
     .pipe(imagemin({
         progressive: true,
@@ -67,35 +71,41 @@ gulp.task('image',['del-image'], function() {
         use: [pngquant()]
     }))
     .pipe(gulp.dest('_site/content/images/'));
-});
+	  cb();
+}
 
 /**
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
  */
-gulp.task('sass', function () {
-    return gulp.src('assets/css/screen.scss')
+function sassTask() {
+    return src('assets/css/screen.scss')
         .pipe(sass())
         .pipe(gulp.dest('assets/css'))
         .pipe(gulp.dest('_site/assets/css/'))
         .pipe(browserSync.reload({stream:true}))
-});
+}
 
 /**
  * Watch scss files for changes & recompile
  * Watch html/md files, run jekyll & reload BrowserSync
  */
-gulp.task('watch', function () {
-    gulp.watch('assets/css/*.scss', ['sass']);
-    gulp.watch(['*.md', '_layouts/*.html', '_includes/*.html', '_posts/*', '_drafts/*'], ['jekyll-rebuild']);
-});
+function watchTask() {
+	watch('assets/css/*.scss', sassTask);
+	watch(['*.md', '_layouts/*.html', '_includes/*.html', '_posts/*', '_drafts/*'], series(jekyllBuildTask, jekyllRebuildTask));
+}
 
 /**
  * Default task, running just `gulp` will compile the sass,
  * compile the jekyll site, launch BrowserSync & watch files.
- */ 
-gulp.task('default', ['browser-sync', 'watch']);
+ */
 
 // 本番環境にのみ残しておきたい記事があるため、完全同期のhookではなく、ファイル追記のみ行うglynnでデプロイします。
 // gulp.task('deploy', function (callback) {
 //     runSequence('jekyll-build', 'add', 'commit', 'push', callback);
 // });
+
+exports.image = series(delImageTask, imageTask)
+exports.default = parallel(
+	series(sassTask, jekyllBuildTask, browserSyncTask),
+	watchTask
+)
